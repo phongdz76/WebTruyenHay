43783a34517dfb1d948f5dbe0a44634607cf6201
phongnguyen <145427852+phongdz76@users.noreply.github.com>
2024-10-11 22:08:53 +0700

    Delete WebTruyenHay directory

--- WebTruyenHay/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace WebTruyenHay.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- WebTruyenHay/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace WebTruyenHay.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
